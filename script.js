f526be331bf8613cb4e592998ffe7eb852e6bf1d
awtnb_laptop <arikaebanataw@gmail.com>
2021-06-20 12:58:30 +0900

    split section: get-reading

--- script.js
+++ script.js
@@ -3,34 +3,34 @@
 // 汎用関数
 ////////////////////////////////////////////////
 
-function escapeMeta(str) {
-    return str.replace(/[-[\]{}()*+?.,\\^$|]/g, "\\$&");
-}
+// function escapeMeta(str) {
+//     return str.replace(/[-[\]{}()*+?.,\\^$|]/g, "\\$&");
+// }
 
-function toHankaku(str) {
-    return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
-        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-    });
-}
+// function toHankaku(str) {
+//     return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+//         return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+//     });
+// }
 
-function copyTable(tableElem) {
-    const range = document.createRange();
-    range.selectNodeContents(tableElem);
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
-    document.execCommand("Copy");
-    alert("コピーしました！");
-}
+// function copyTable(tableElem) {
+//     const range = document.createRange();
+//     range.selectNodeContents(tableElem);
+//     const selection = window.getSelection();
+//     selection.removeAllRanges();
+//     selection.addRange(range);
+//     document.execCommand("Copy");
+//     alert("コピーしました！");
+// }
 
-function resetTable(tableElem) {
-    const maxRow = tableElem.rows.length;
-    if (maxRow > 1) {
-        for (let r = maxRow - 1; r >= 1; r--) {
-            tableElem.deleteRow(r);
-        }
-    }
-}
+// function resetTable(tableElem) {
+//     const maxRow = tableElem.rows.length;
+//     if (maxRow > 1) {
+//         for (let r = maxRow - 1; r >= 1; r--) {
+//             tableElem.deleteRow(r);
+//         }
+//     }
+// }
 
 ////////////////////////////////////////////////
 // 読み取得
