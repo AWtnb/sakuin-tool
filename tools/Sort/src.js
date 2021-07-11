f11cc7e0807597b6e2ffcb4d7ec04a460e1d2eff
awtnb_laptop <arikaebanataw@gmail.com>
2021-07-11 14:26:40 +0900

    move js to external file

---
+++ tools/Sort/src.js
@@ -0,0 +1,27 @@
+function fromTsv (tsv) {
+    const lines = tsv.split(/[\r\n]+/g).filter(line => line);
+    return lines.map(line => {
+        const [item, reading, norm,  ...rest] = line.split("\t"); // 配列読み＝正規化＝normalized
+        return {
+           Item: item,
+           Reading: reading,
+           Norm: norm
+        }
+    });
+}
+
+function comparer (a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+}
+
+function sortByReading (obj) {
+    return obj.sort((a,b) => {
+        return comparer(a.Item, b.Item);
+    }).sort((a,b) => {
+        return comparer(a.Reading, b.Reading);
+    }).sort((a,b) => {
+        return comparer(a.Norm, b.Norm);
+    });
+}
