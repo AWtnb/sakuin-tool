be9548c868e560d7721906744146e7dcd58f24b8
awtnb_laptop <arikaebanataw@gmail.com>
2021-10-15 16:31:59 +0900

    sort result in original order

--- tools/FindNombreToRemove/src.js
+++ tools/FindNombreToRemove/src.js
@@ -3,12 +3,23 @@ function grepNombredReference(lines) {
     return lines.filter(line => line.match(/→.*\d/));
 }
 
-function grepItemToAddNombre(lines) {
-    return lines.map(line => line.replace(/^.+?→\s*/, "")).map(line => {
+function grepItemToAddNombre(lines, references) {
+    const refMap = new Map();
+    references.map(line => line.replace(/^.+?→\s*/, "")).forEach(line => {
         const [item, nombre, ...rest] = line.split("　　");
+        if (refMap.has(item)) {
+            refMap.set(item, refMap.get(item).concat(nombre))
+        }
+        else {
+            refMap.set(item, [nombre]);
+        }
+    });
+    const baseItems = lines.map(line => line.replace(/(（.+?）)?　　.+$/, ""));
+    const uniq = Array.from(new Set(baseItems));
+    return uniq.filter(item => refMap.has(item)).map(line => {
         return {
-            "ReferTo": item,
-            "Nombre": nombre
+            "ReferTo": line,
+            "NombreToAdd": refMap.get(line)
         }
     });
 }
