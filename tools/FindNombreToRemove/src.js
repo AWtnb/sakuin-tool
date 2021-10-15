b9f2a2e6484e5cdb341953284c6382c84594e77c
awtnb_laptop <arikaebanataw@gmail.com>
2021-10-15 15:26:52 +0900

    add grep function

--- tools/FindNombreToRemove/src.js
+++ tools/FindNombreToRemove/src.js
@@ -2,3 +2,13 @@
 function grepNombredReference(lines) {
     return lines.filter(line => line.match(/→.*\d/));
 }
+
+function grepItemToAddNombre(lines) {
+    return lines.map(line => line.replace(/^.+?→\s*/)).map(line => {
+        const [item, nombre, ...rest] = line.split("　　");
+        return {
+            "ReferTo": item,
+            "Nombre": nombre
+        }
+    });
+}
