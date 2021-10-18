5b1879ba40cbe23ab7de40da81cf0c8ab60a8ee9
awtnb_laptop <arikaebanataw@gmail.com>
2021-10-18 15:12:27 +0900

    add grouping tool

---
+++ tools/FindDuplicated/src.js
@@ -0,0 +1,13 @@
+
+function getReferencesToGroup(lines) {
+    const nonReferred = lines.filter(Boolean).filter(line => !line.match(/（.+?）/));
+    const referred = lines.filter(Boolean).filter(line => line.match(/（.+?）/));
+    return referred.map(line => {
+        const basename = line.replace(/（.+?）.+$/, "");
+        const grep = nonReferred.filter(line => line.replace(/　　.+$/, "") == basename);
+        if (grep.length > 0) {
+            return [line].concat(grep);
+        }
+        return null;
+    }).filter(Boolean);
+}
