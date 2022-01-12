da1629d7bc7825c8ccf2f66bba05962c73a9b12e
awtnb_laptop <arikaebanataw@gmail.com>
2022-01-12 22:45:37 +0900

    fix check of nayose

--- tools/CheckNayose/src.js
+++ tools/CheckNayose/src.js
@@ -1,10 +1,23 @@
 
-function getDuplicatedItemsInNayose(lines) {
-    const nonReferred = lines.filter(Boolean).filter(line => !line.match(/（.+?）/));
+function getUngroupedReferencedItems(lines) {
     const referred = lines.filter(Boolean).filter(line => line.match(/（.+?）/));
+    const nonReferred = lines.filter(Boolean).filter(line => !referred.includes(line));
     return referred.map(line => {
         const basename = line.replace(/（.+?）.+$/, "");
         const grep = nonReferred.filter(line => line.replace(/　　.+$/, "") == basename);
+        if (grep.length > 0) {
+            return [line].concat(grep);
+        }
+        return null;
+    }).filter(Boolean);
+}
+
+function getConflictReferringItems(lines) {
+    const referring = lines.filter(Boolean).filter(line => line.match(/\s+→/));
+    const nonReferring = lines.filter(Boolean).filter(line => !referring.includes(line));
+    return referring.map(line => {
+        const basename = line.replace(/\s*→.+$/, "");
+        const grep = nonReferring.filter(line => line.replace(/(（.+?）)?　　.+$/, "") == basename);
         if (grep.length > 0) {
             return [line].concat(grep);
         }
