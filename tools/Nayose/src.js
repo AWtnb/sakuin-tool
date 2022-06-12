16aacf2beb9b336ea954c135a35a525a7a9de45c
awtnb_laptop <arikaebanataw@gmail.com>
2022-06-12 17:53:32 +0900

    fix nombre class

--- tools/Nayose/src.js
+++ tools/Nayose/src.js
@@ -1,68 +1,3 @@
-class NombreGroup {
-
-    constructor(s) {
-        this.parsed = new Nombre(s).parse();
-    }
-
-    static isConsecutive (a, b, c) {
-        if (a.intValue+1 == b.intValue && a.intValue+2 == c.intValue) {
-            if (String(b.display.text).match(/^\d+$/)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    order() {
-        this.parsed = this.parsed.filter(x => x.display.text).sort((a, b) => a.intValue - b.intValue);
-    }
-
-    unify() {
-        const stack = [];
-        this.parsed = this.parsed.filter(nombre => {
-            if (stack.includes(nombre.display.text)) {
-                return false;
-            }
-            stack.push(nombre.display.text);
-            return true;
-        });
-    }
-
-    hyphenate() {
-        if (this.parsed.length < 3) {
-            return;
-        }
-        const stack = [];
-        stack.push({
-            "item": this.parsed[0],
-            "isHyphen": false
-        });
-        for (let i = 0; i < this.parsed.length - 2; i++) {
-            const current = this.parsed[i];
-            const next1 = this.parsed[i+1];
-            const next2 = this.parsed[i+2];
-            stack.push({
-                "item": next1,
-                "isHyphen": NombreGroup.isConsecutive(current, next1, next2)
-            });
-        }
-        stack.push({
-            "item": this.parsed.slice(-1)[0],
-            "isHyphen": false
-        });
-        this.parsed = stack.map(x => {
-            if (x.isHyphen) {
-                x.item.display.text = "\u2013";
-            }
-            return x.item;
-        });
-    }
-
-    toString() {
-        return this.parsed.map(p => p.display.text).join(", ").replace(/, (\u2013, )+/g, "\u2013");
-    }
-
-}
 
 function parseLine(s, nombreOnLeft = false) {
     const arr = s.split("\t").slice(0, 2).map(x => String(x).trim());
@@ -95,11 +30,8 @@ function nayose (lines, nombreOnLeft = false) {
     });
     const ret = [];
     map.forEach((nombres, item) => {
-        const parsed = new NombreGroup(nombres);
-        parsed.order();
-        parsed.unify();
-        parsed.hyphenate();
-        ret.push((item + "　　" + parsed.toString()).trim());
+        const parsed = new Nombre(nombres);
+        ret.push((item + "　　" + parsed.format()).trim());
     });
     return ret;
 }
@@ -130,11 +62,8 @@ function nayoseByOrder(lines, nombreOnLeft = false) {
     }
 
     return stack.map(pair => {
-        const parsed = new NombreGroup(pair.Nombres);
-        parsed.order();
-        parsed.unify();
-        parsed.hyphenate();
-        return (pair.Item + "　　" + parsed.toString()).trim();
+        const parsed = new Nombre(pair.Nombres);
+        return (pair.Item + "　　" + parsed.format()).trim();
     });
 
 }
--- tools/nombre.js
+++ tools/nombre.js
@@ -1,29 +1,22 @@
-class Nombre {
+class NombreParser {
 
-    constructor(s) {
-        this.sanitized = Nombre.sanitize(s);
-        this.Elements = this.sanitized.split(",").map(x => x.trim()).filter(Boolean);
-    }
-
-    parse() {
-        /**
-         * nombre: must be string sepatated by comma
-         */
+    static parse(s) {
         const stack = [];
-        this.Elements.map(s => {
-            if (String(s).indexOf("-") != -1) {
-                const [start, end, ...rest] = s.split("-");
-                Nombre.parseRange(start, end).forEach(x => stack.push(x));
+        s.split(",").map(x => x.trim()).filter(Boolean).map(nombre => {
+            const nStr = String(nombre);
+            if (nStr.indexOf("-") != -1) {
+                const [start, end, ...rest] = nStr.split("-");
+                NombreParser.parseRange(start, end).forEach(x => stack.push(x));
             }
             else {
                 stack.push({
                     "display": {
-                        "text": String(s),
-                        "prefix": Nombre.getPrefix(s),
-                        "suffix": Nombre.getSuffix(s),
+                        "text": nStr,
+                        "prefix": NombreParser.getPrefix(nStr),
+                        "suffix": NombreParser.getSuffix(nStr),
                     },
-                    "intValue": Number(s.replace(/[^\d]/g, "")),
-                    "hyphenated": false
+                    "intValue": Number(nStr.replace(/[^\d]/g, "")),
+                    "hyphenated": false,
                 });
             }
         });
@@ -31,7 +24,7 @@ class Nombre {
     }
 
     static sanitize(s) {
-        return s.replace(/\s/g, "").replace(/\uff0c/g, ",").replace(/[\u002d\u2010\u2011\u2012\u2013\u2014\uFF0D]/g, "-").replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(m) {
+        return s.replace(/\s/g, "").replace(/\uff0c/g, ",").replace(/[\u002d\u2010\u2011\u2012\u2013\u2014\uFF0D]/g, "-").replace(/\-+/g, "-").replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(m) {
             return String.fromCharCode(m.charCodeAt(0) - 0xFEE0);
         });
     }
@@ -52,11 +45,11 @@ class Nombre {
 
     static parseRange(start, end) {
         const range = [];
-        const s = new Nombre(start).parse()[0];
-        const e = new Nombre(end).parse()[0];
+        const s = NombreParser.parse(String(start))[0];
+        const e = NombreParser.parse(String(end))[0];
         range.push(s);
         for (let idx = s.intValue + 1; idx < e.intValue; idx++) {
-            const p = new Nombre(String(idx)).parse()[0];
+            const p = NombreParser.parse(String(idx))[0];
             p.hyphenated = true;
             range.push(p);
         }
@@ -65,3 +58,84 @@ class Nombre {
     }
 
 }
+
+class Nombre {
+
+    constructor(s) {
+        const sanitized = NombreParser.sanitize(s);
+        this.RawElements = sanitized.split(",").map(x => x.trim()).filter(Boolean);
+        this.Parsed = NombreParser.parse(sanitized);
+    }
+
+    isConsecutiveTriplet(startIdx) {
+        if (this.Parsed.length - 3 < startIdx) {
+            return false;
+        }
+        const current = this.Parsed[startIdx];
+        const next1 = this.Parsed[startIdx+1];
+        const next2 = this.Parsed[startIdx+2];
+        if (current.intValue+1 == next1.intValue && current.intValue+2 == next2.intValue) {
+            if (String(next1.display.text).match(/^\d+$/)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    order() {
+        const p = this.Parsed;
+        this.Parsed = p.filter(x => x.display.text).sort((a, b) => a.intValue - b.intValue);
+        return this.Parsed;
+    }
+
+    unify() {
+        const p = this.Parsed;
+        const stack = [];
+        this.Parsed = p.filter(nombre => {
+            if (stack.includes(nombre.display.text)) {
+                return false;
+            }
+            stack.push(nombre.display.text);
+            return true;
+        });
+        return this.Parsed;
+    }
+
+    hyphenate() {
+        if (this.Parsed.length < 3) {
+            return;
+        }
+        const stack = [];
+        stack.push({
+            "item": this.Parsed[0],
+            "isHyphen": false
+        });
+        for (let i = 0; i < this.Parsed.length - 2; i++) {
+            const next = this.Parsed[i+1];
+            stack.push({
+                "item": next,
+                "isHyphen": this.isConsecutiveTriplet(i)
+            });
+        }
+        stack.push({
+            "item": this.Parsed.slice(-1)[0],
+            "isHyphen": false
+        });
+        this.Parsed = stack.map(x => {
+            if (x.isHyphen) {
+                x.item.display.text = "\u2013";
+            }
+            return x.item;
+        });
+        return this.Parsed;
+    }
+
+    format() {
+        this.order();
+        this.unify();
+        this.hyphenate();
+        return this.Parsed.map(p => p.display.text).join(", ").replace(/, (\u2013, )+/g, "\u2013");
+    }
+
+}
+
