00504d97cb2ed3c9ef844e4eb340ba39630b0567
awtnb_laptop <arikaebanataw@gmail.com>
2022-06-18 23:45:46 +0900

    wrap inside class

--- tools/Sort/src.js
+++ tools/Sort/src.js
@@ -1,29 +1,33 @@
-function fromTsv (tsv) {
-    const lines = Util.linesArray(tsv).filter(line => line);
-    return lines.map(line => {
-        const [item, reading, norm,  ...rest] = line.split("\t"); // 配列読み＝正規化＝normalized
-        return {
-           "Item": item,
-           "Reading": reading,
-           "Norm": norm
-        }
-    });
-}
+class SortIndex {
 
-function comparer (a, b) {
-    const aLower = String(a).toLowerCase();
-    const bLower = String(b).toLowerCase();
-    if (aLower > bLower) return 1;
-    if (aLower < bLower) return -1;
-    return 0;
-}
+    static fromTsv (tsv) {
+        const lines = Util.linesArray(tsv).filter(line => line);
+        return lines.map(line => {
+            const [item, reading, norm,  ...rest] = line.split("\t"); // 配列読み＝正規化＝normalized
+            return {
+               "Item": item,
+               "Reading": reading,
+               "Norm": norm
+            }
+        });
+    }
 
-function sortByReading (obj) {
-    return obj.sort((a,b) => {
-        return comparer(a.Item, b.Item);
-    }).sort((a,b) => {
-        return comparer(a.Reading, b.Reading);
-    }).sort((a,b) => {
-        return comparer(a.Norm, b.Norm);
-    });
+    static comparer (a, b) {
+        const aLower = String(a).toLowerCase();
+        const bLower = String(b).toLowerCase();
+        if (aLower > bLower) return 1;
+        if (aLower < bLower) return -1;
+        return 0;
+    }
+
+    static sortByReading (obj) {
+        return obj.sort((a,b) => {
+            return SortIndex.comparer(a.Item, b.Item);
+        }).sort((a,b) => {
+            return SortIndex.comparer(a.Reading, b.Reading);
+        }).sort((a,b) => {
+            return SortIndex.comparer(a.Norm, b.Norm);
+        });
+    }
+
 }
