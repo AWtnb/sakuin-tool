fdb698df5f13a4025240c5302454cb0be4fcb345
awtnb_laptop <arikaebanataw@gmail.com>
2022-06-18 22:04:11 +0900

    fix

--- tools/CheckChild/src.js
+++ tools/CheckChild/src.js
@@ -1,59 +1,63 @@
-function getMainEntries(lines) {
-    return lines.filter(x => String(x).trim()).map(line => {
-        const p = Entry.parse(line);
-        if (!p.isReference && !p.isChild) {
-            return {
-                "name": p.name,
-                "basename": p.basename,
-                "subInfo": p.name.slice(p.basename.length)
-            };
-        }
-        return null;
-    }).filter(Boolean);
-}
+class AdjustNombre {
 
-function markupHead(line, search) {
-    if (line.startsWith(search)) {
-        return `<span class="match">${search}</span>` + line.slice(search.length);
+    static getMainEntries(lines) {
+        return lines.filter(x => String(x).trim()).map(line => {
+            const p = Entry.parse(line);
+            if (!p.isReference && !p.isChild) {
+                return {
+                    "name": p.name,
+                    "basename": p.basename,
+                    "subInfo": p.name.slice(p.basename.length)
+                };
+            }
+            return null;
+        }).filter(Boolean);
     }
-    return line;
-}
 
-function markupTail(line, search) {
-    if (line.endsWith(search)) {
-        return line.slice(0, 0 - search.length) + `<span class="match">${search}</span>`;
-    }
-    return line;
-}
-
-function findPossibleChildItems(lines, mode = "tail") {
-    const params = {
-        "head": {"markupHead": true, "markupTail": false},
-        "tail": {"markupHead": false, "markupTail": true},
-        "all": {"markupHead": true, "markupTail": true}
-    }[mode];
-    const mainEntries = getMainEntries(lines);
-    return mainEntries.map(entry => {
-        const search = entry.basename;
-        const possibles = mainEntries.filter(entry => entry.basename != search).map(entry => {
-            let markup = entry.basename;
-            if (params.markupHead) {
-                markup = markupHead(markup, search);
-            }
-            if (params.markupTail) {
-                markup = markupTail(markup, search);
-            }
-            return {
-                "Markup": markup + entry.subInfo,
-                "Changed": entry.basename != markup
-            }
-        }).filter(x => x.Changed);
-        if (possibles.length > 0) {
-            return {
-                "Found": search,
-                "Markup": possibles.map(p => `・${p.Markup}`).join("<br>")
-            }
+    static markupHead(line, search) {
+        if (line.startsWith(search)) {
+            return `<span class="match">${search}</span>` + line.slice(search.length);
         }
-        return null;
-    }).filter(Boolean).sort((a, b) => b.Found.length - a.Found.length);
+        return line;
+    }
+
+    static markupTail(line, search) {
+        if (line.endsWith(search)) {
+            return line.slice(0, 0 - search.length) + `<span class="match">${search}</span>`;
+        }
+        return line;
+    }
+
+    static findPossibleChildItems(lines, mode = "tail") {
+        const params = {
+            "head": {"markupHead": true, "markupTail": false},
+            "tail": {"markupHead": false, "markupTail": true},
+            "all": {"markupHead": true, "markupTail": true}
+        }[mode];
+        const mainEntries = this.constructor.getMainEntries(lines);
+        return mainEntries.map(entry => {
+            const search = entry.basename;
+            const possibles = mainEntries.filter(entry => entry.basename != search).map(entry => {
+                let markup = entry.basename;
+                if (params.markupHead) {
+                    markup = this.constructor.markupHead(markup, search);
+                }
+                if (params.markupTail) {
+                    markup = this.constructor.markupTail(markup, search);
+                }
+                return {
+                    "Markup": markup + entry.subInfo,
+                    "Changed": entry.basename != markup
+                }
+            }).filter(x => x.Changed);
+            if (possibles.length > 0) {
+                return {
+                    "Found": search,
+                    "Markup": possibles.map(p => `・${p.Markup}`).join("<br>")
+                }
+            }
+            return null;
+        }).filter(Boolean).sort((a, b) => b.Found.length - a.Found.length);
+    }
+
 }
