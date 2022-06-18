6b4f592794f649c56859634e77cd4263516ac8cf
awtnb_laptop <arikaebanataw@gmail.com>
2022-06-18 23:57:30 +0900

    wrap in class

--- tools/GetReading/src.js
+++ tools/GetReading/src.js
@@ -1,24 +1,26 @@
-function getReading(token) {
-    console.log(token.surface_form, token.word_type, token.pos, token.reading);
-    const surface = token.surface_form;
-    if (surface.match(/([a-zA-Z]|[ァ-ヴー・])+/g)) {
-        return surface;
-    }
-    if (token.pos == "記号") {
-        return surface;
-    }
-    if (token.word_type != "KNOWN") {
-        return token.surface_form;
-    }
-    return token.reading;
-}
-
-function tokenizeLines(tokenizer, lines) {
-    return lines.map(line => {
-        const reading = tokenizer.tokenize(line).map( getReading ).join("");
-        return {
-           "Item": line,
-           "Reading": reading
+class Reading {
+    static getReading(token) {
+        console.log(token.surface_form, token.word_type, token.pos, token.reading);
+        const surface = token.surface_form;
+        if (surface.match(/([a-zA-Z]|[ァ-ヴー・])+/g)) {
+            return surface;
         }
-    });
+        if (token.pos == "記号") {
+            return surface;
+        }
+        if (token.word_type != "KNOWN") {
+            return token.surface_form;
+        }
+        return token.reading;
+    }
+
+    static tokenizeLines(tokenizer, lines) {
+        return lines.map(line => {
+            const reading = tokenizer.tokenize(line).map( Reading.getReading ).join("");
+            return {
+                "Item": line,
+                "Reading": reading
+            }
+        });
+}
 }
