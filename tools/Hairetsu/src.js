9929b46533cc3caa255f5a4c55008929a668fb98
awtnb_laptop <arikaebanataw@gmail.com>
2022-06-18 23:51:21 +0900

    wrap in class

--- tools/Hairetsu/src.js
+++ tools/Hairetsu/src.js
@@ -1,20 +1,22 @@
-function toHairetsu (s, removeNoise) {
-    const map = new Map();
-    [
-        ["ァ", "ア"], ["ィ", "イ"], ["ゥ", "ウ"], ["ェ", "エ"],["ォ", "オ"], ["ヴ", "ウ"],
-        ["ガ", "カ"], ["ギ", "キ"], ["グ", "ク"], ["ゲ", "ケ"],["ゴ", "コ"],
-        ["ザ", "サ"], ["ジ", "シ"], ["ズ", "ス"], ["ゼ", "セ"],["ゾ", "ソ"],
-        ["ダ", "タ"], ["ヂ", "チ"], ["ヅ", "ツ"], ["ッ", "ツ"],["デ", "テ"], ["ド", "ト"],
-        ["バ", "ハ"], ["ビ", "ヒ"], ["ブ", "フ"], ["ベ", "ヘ"],["ボ", "ホ"],
-        ["パ", "ハ"], ["ピ", "ヒ"], ["プ", "フ"], ["ペ", "ヘ"],["ポ", "ホ"],
-        ["ャ", "ヤ"], ["ュ", "ユ"], ["ョ", "ヨ"],
-        ["ー", ""]
-    ].forEach(x => map.set(...x));
+class NormalizedReading {
+    static convert (s, removeNoise) {
+        const map = new Map();
+        [
+            ["ァ", "ア"], ["ィ", "イ"], ["ゥ", "ウ"], ["ェ", "エ"],["ォ", "オ"], ["ヴ", "ウ"],
+            ["ガ", "カ"], ["ギ", "キ"], ["グ", "ク"], ["ゲ", "ケ"],["ゴ", "コ"],
+            ["ザ", "サ"], ["ジ", "シ"], ["ズ", "ス"], ["ゼ", "セ"],["ゾ", "ソ"],
+            ["ダ", "タ"], ["ヂ", "チ"], ["ヅ", "ツ"], ["ッ", "ツ"],["デ", "テ"], ["ド", "ト"],
+            ["バ", "ハ"], ["ビ", "ヒ"], ["ブ", "フ"], ["ベ", "ヘ"],["ボ", "ホ"],
+            ["パ", "ハ"], ["ピ", "ヒ"], ["プ", "フ"], ["ペ", "ヘ"],["ポ", "ホ"],
+            ["ャ", "ヤ"], ["ュ", "ユ"], ["ョ", "ヨ"],
+            ["ー", ""]
+        ].forEach(x => map.set(...x));
 
-    let katakana = Util.toHalfWidth(Util.toKatakana(s));
-    for (let k of map.keys()) {
-        const reg = new RegExp(k, "g");
-        katakana = katakana.replace(reg, map.get(k));
-    }
-    return (removeNoise)? katakana.replace(/[^ァ-ヴa-zA-Zａ-ｚＡ-Ｚ0-9０-９\r\n]/g, "") : katakana;
+        let katakana = Util.toHalfWidth(Util.toKatakana(s));
+        for (let k of map.keys()) {
+            const reg = new RegExp(k, "g");
+            katakana = katakana.replace(reg, map.get(k));
+        }
+        return (removeNoise)? katakana.replace(/[^ァ-ヴa-zA-Zａ-ｚＡ-Ｚ0-9０-９\r\n]/g, "") : katakana;
+}
 }
