e76365f48a25900c43694d08f9fd66faa2c958c4
awtnb_laptop <arikaebanataw@gmail.com>
2022-06-19 00:34:25 +0900

    fix with class

--- tools/CheckGrouping/src.js
+++ tools/CheckGrouping/src.js
@@ -1,28 +1,29 @@
 class GroupingChecker {
 
-    static getUngrouped(lines) {
-        const entries = lines.filter(x => String(x).trim()).map(line => Entry.parse(line));
-        const refs = entries.filter(entry => entry.isReference);
-        const nonReferred = entries.filter(entry => entry.referredFrom.length < 1);
-        return refs.map(entry => {
-            const referTo = entry.referTo;
-            const grep = nonReferred.filter(entry => entry.basename == referTo);
-            if (grep.length > 0) {
-                return [entry.name].concat(grep.map(entry => entry.name));
+    constructor(lines) {
+        this.entries = lines.filter(x => String(x).trim()).map(line => Entry.parse(line));
+        this.refs = this.entries.filter(entry => entry.isReference);
+        this.nonRefs = this.entries.filter(entry => !entry.isReference);
+        this.nonReferred = this.entries.filter(entry => entry.referredFrom.length < 1);
+    }
+
+    getUngrouped() {
+        return this.refs.map(entry => {
+            const refTo = entry.referTo;
+            const found = this.nonReferred.filter(entry => entry.basename == refTo);
+            if (found.length > 0) {
+                return [entry.name].concat(found.map(entry => entry.name));
             }
             return null;
         }).filter(Boolean);
     }
 
-    static getConflicting(lines) {
-        const entries = lines.filter(x => String(x).trim()).map(line => Entry.parse(line));
-        const refs = entries.filter(entry => entry.isReference);
-        const nonReferring = entries.filter(entry => !entry.isReference);
-        return refs.map(entry => {
-            const basename = entry.basename;
-            const grep = nonReferring.filter(entry => entry.basename == basename);
-            if (grep.length > 0) {
-                return [entry.name].concat(grep.map(entry => entry.name));
+    getConflicting() {
+        return this.refs.map(entry => {
+            const refFrom = entry.basename;
+            const found = this.nonRefs.filter(entry => entry.basename == refFrom);
+            if (found.length > 0) {
+                return [entry.name].concat(found.map(entry => entry.name));
             }
             return null;
         }).filter(Boolean);
