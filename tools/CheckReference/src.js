028fded6f72e719b7031c132c2d83c4812e1f29a
awtnb_laptop <arikaebanataw@gmail.com>
2022-07-26 23:35:47 +0900

    unify check tool

--- tools/CheckReference/src.js
+++ tools/CheckReference/src.js
@@ -48,7 +48,7 @@ export class ReferenceChecker {
             return {
                 "problem": line.text,
                 "require": required.map(s => {
-                    return { "from": s, "to":line.text };
+                    return { "from": s, "to":line.basename };
                 })
             };
         }).filter(Boolean);
@@ -73,5 +73,44 @@ export class ReferenceChecker {
         return stack.map(entry => entry.name);
     }
 
+
+}
+
+export class GroupingChecker {
+
+    constructor(lines) {
+        this.entries = lines.filter(x => String(x).trim()).map(line => new Entry(line));
+        this.refs = this.entries.filter(entry => entry.isReference);
+        this.nonRefs = this.entries.filter(entry => !entry.isReference);
+        this.nonReferred = this.entries.filter(entry => entry.referredFrom.length < 1);
+    }
+
+    getUngrouped() {
+        return this.refs.map(entry => {
+            const refTo = entry.referTo;
+            const found = this.nonReferred.filter(entry => entry.basename == refTo);
+            if (found.length > 0) {
+                return {
+                    "name": entry.name,
+                    "detail": found.map(entry => entry.rawStr)
+                };
+            }
+            return null;
+        }).filter(Boolean);
+    }
+
+    getConflicting() {
+        return this.refs.map(entry => {
+            const refFrom = entry.basename;
+            const found = this.nonRefs.filter(entry => entry.basename == refFrom);
+            if (found.length > 0) {
+                return {
+                    "name": entry.name,
+                    "detail": found.map(entry => entry.rawStr)
+                };
+            }
+            return null;
+        }).filter(Boolean);
+    }
 
 }
