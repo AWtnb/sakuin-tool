8325eecf188e8b97df8e0c3f34b1bab298e7bbc3
awtnb_laptop <arikaebanataw@gmail.com>
2022-07-27 22:37:52 +0900

    unify tokenizing

--- tools/GetReading/src.js
+++ tools/GetReading/src.js
@@ -1,3 +1,26 @@
+import {Util, Entry} from "../common.js";
+
+function normalize(s, removeNoise) {
+    const map = new Map();
+    [
+        ["ァ", "ア"], ["ィ", "イ"], ["ゥ", "ウ"], ["ェ", "エ"],["ォ", "オ"], ["ヴ", "ウ"],
+        ["ガ", "カ"], ["ギ", "キ"], ["グ", "ク"], ["ゲ", "ケ"],["ゴ", "コ"],
+        ["ザ", "サ"], ["ジ", "シ"], ["ズ", "ス"], ["ゼ", "セ"],["ゾ", "ソ"],
+        ["ダ", "タ"], ["ヂ", "チ"], ["ヅ", "ツ"], ["ッ", "ツ"],["デ", "テ"], ["ド", "ト"],
+        ["バ", "ハ"], ["ビ", "ヒ"], ["ブ", "フ"], ["ベ", "ヘ"],["ボ", "ホ"],
+        ["パ", "ハ"], ["ピ", "ヒ"], ["プ", "フ"], ["ペ", "ヘ"],["ポ", "ホ"],
+        ["ャ", "ヤ"], ["ュ", "ユ"], ["ョ", "ヨ"],
+        ["ー", ""]
+    ].forEach(x => map.set(...x));
+
+    let katakana = Util.toHalfWidth(Util.toKatakana(s));
+    for (let k of map.keys()) {
+        const reg = new RegExp(k, "g");
+        katakana = katakana.replace(reg, map.get(k));
+    }
+    return (removeNoise)? katakana.replace(/[^ァ-ヴa-zA-Z\uff41-\uff5a\uff21-\uff3a0-9\uff10-\uff19\r\n]/g, "") : katakana;
+
+}
 
 function getReading(tokens) {
     return tokens.reduce((accum, token) => {
@@ -17,15 +40,19 @@ function getReading(tokens) {
 }
 
 export class Reading {
-    constructor(tokenizer) {
+    constructor(tokenizer, removeNoise=true) {
         this.tokenizer = tokenizer;
+        this.removeNoise = removeNoise;
     }
     parseLines(lines) {
         return lines.map(line => {
-            const tokens = this.tokenizer.tokenize(line);
+            const target = new Entry(line).name;
+            const tokens = this.tokenizer.tokenize(target);
+            const r = getReading(tokens);
             return {
                 "item": line,
-                "reading": getReading(tokens)
+                "reading": r,
+                "normalized": normalize(r, this.removeNoise)
             }
         });
     }
