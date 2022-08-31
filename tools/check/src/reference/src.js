cea13bdd68b51cd23ac0d72eb3d4554ead0e1ce1
awtnb_laptop <arikaebanataw@gmail.com>
2022-08-31 15:47:22 +0900

    fix searchng

--- tools/check/src/reference/src.js
+++ tools/check/src/reference/src.js
@@ -70,36 +70,41 @@ export class GroupingChecker {
         this.entries = lines.filter(x => String(x).trim()).map(line => new Entry(line));
         this.refs = this.entries.filter(entry => entry.isReference);
         this.nonRefs = this.entries.filter(entry => !entry.isReference);
+        this.mainEntries = this.nonRefs.filter(entry => !entry.isChild);
         this.referred = this.entries.filter(entry => entry.referredFrom.length > 0);
         this.nonReferred = this.entries.filter(entry => entry.referredFrom.length < 1);
     }
 
     getUngrouped() {
-        return this.refs.map(entry => {
-            const refTo = entry.referTo;
-            const found = this.nonReferred.filter(entry => entry.basename == refTo);
-            if (found.length > 0 && this.referred.filter(entry => entry.basename == refTo).length > 0) {
-                return {
-                    "name": entry.name,
-                    "detail": found.map(entry => entry.rawStr)
-                };
+        // basename が同じものが2つ以上ないか（括弧の有無だけで別項目扱いになっていないか）探す
+        const stack = [];
+        this.mainEntries.forEach(entry => {
+            const target = entry.basename;
+            const grep = this.mainEntries.filter(e => e.basename == target);
+            if (grep.length == 1) {
+                return;
             }
-            return null;
-        }).filter(Boolean);
+            if (!stack.map(x => x.id).includes(target)) {
+                stack.push({
+                    "id": target,
+                    "detail": grep.map(entry => entry.rawStr)
+                });
+            }
+        });
+        return stack.map(x => x.detail);
     }
 
     getConflicting() {
+        // 本項目として残っている見よ項目を探す
         return this.refs.map(entry => {
-            const refFrom = entry.basename;
-            const found = this.nonRefs.filter(entry => entry.basename == refFrom);
-            if (found.length > 0) {
-                return {
-                    "name": entry.name,
-                    "detail": found.map(entry => entry.rawStr)
-                };
+            const target = entry.basename;
+            const grep = this.mainEntries.filter(e => e.basename == target);
+            if (grep.length < 1) {
+                return null;
             }
-            return null;
+            return [entry.name].concat(grep.map(e => e.rawStr))
         }).filter(Boolean);
+
     }
 
 }
