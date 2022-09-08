a532d7d325ce901e746d227e461e0a436a5a22d4
awtnb_laptop <arikaebanataw@gmail.com>
2022-09-08 18:08:47 +0900

    fix adjusting nombres

--- tools/adjuster/src.js
+++ tools/adjuster/src.js
@@ -1,62 +1,65 @@
 import {Entry, AddressHandler} from "../../assets/common.js";
 
-export class NombreAdjuster {
+const pruneNaN = (s) =>  {
+    if (isNaN(Number(s))) {
+        return s.replace(/[^\d]/g, "");
+    }
+    return s;
+}
+
+const compareNombres = (nombresA, nombresB) => {
+    const detail = nombresA.map((nbrA, idx) => {
+        const nbrB = nombresB[idx];
+        if (pruneNaN(nbrA) != pruneNaN(nbrB)) {
+            return {
+                "text": nbrB,
+                "before": nbrA,
+                "color": "#ff0080"
+            };
+        }
+        return {
+            "text": nbrA,
+            "before": "",
+            "color": "#333"
+        };
+    });
+    return {
+        "modified": detail.some(x => x.before.length > 0),
+        "detail": detail
+    };
+}
+
+export class EntryLines {
     constructor(start, end, delta) {
         this.start = start;
         this.end = end;
         this.delta = delta;
     }
-    exec(target) {
-        if (Number(this.start) <= target.intValue && target.intValue <= Number(this.end)) {
-            return (target.display.prefix + (target.intValue + Number(this.delta)) + target.display.suffix);
+
+    adjust(entry) {
+        if (Number(this.start) <= entry.intValue && entry.intValue <= Number(this.end)) {
+            return (entry.display.prefix + (entry.intValue + Number(this.delta)) + entry.display.suffix);
         }
-        return target.display.text;
-    }
-}
-
-const stripNAN = (s) =>  {
-    return s.replace(/[^\d]/g, "");
-}
-
-export class EntryLine {
-    constructor(line, adjuster) {
-        const entry = new Entry(line);
-        this.name = entry.name;
-        this.originalNombres = new AddressHandler(entry.address).rawElements;
-        this.adjustedNombres = this.originalNombres.map(nombre => {
-            const parsed = new AddressHandler(nombre).nombres;
-            if (parsed.length > 1) {
-                // 範囲指定のノンブルだった場合
-                return adjuster.exec(parsed[0]) + "\u2013" + adjuster.exec(parsed.at(-1));
-            }
-            return adjuster.exec(parsed[0]);
-        });
+        return entry.display.text;
     }
 
-    compareAddress() {
-        const stack = [];
-        let modified = false;
-        this.originalNombres.forEach((nombre, idx) => {
-            if (stripNAN(nombre) != stripNAN(this.adjustedNombres[idx])) {
-                modified = true;
-                stack.push({
-                    "text": this.adjustedNombres[idx],
-                    "before": nombre,
-                    "color": "#ff0080"
-                });
-            }
-            else {
-                stack.push({
-                    "text": nombre,
-                    "before": "",
-                    "color": "#333"
-                });
-            }
+    load(lines) {
+        return lines.map(line => {
+            const entry = new Entry(line);
+            const orgNombres = new AddressHandler(entry.address).rawElements;
+            const newNombres = orgNombres.map(orgNbr => {
+                const parsed = new AddressHandler(orgNbr).nombres;
+                if (parsed.length > 1) { // 範囲指定のノンブルだった場合
+                    return this.adjust(parsed[0]) + "\u2013" + this.adjust(parsed.at(-1));
+                }
+                return this.adjust(parsed[0]);
+            });
+            return {
+                "name": entry.name,
+                "newNombres": newNombres,
+                "comparison": compareNombres(orgNombres, newNombres)
+            };
         });
-        return {
-            "modified": modified,
-            "detail": stack
-        };
     }
 
 }
