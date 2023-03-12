3009040198e72142210e22b6631307d9eb96fb00
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-12 23:02:59 +0900

    remove unused

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/vue.svg">
-    <!-- <HelloWorld msg="Vite + Vue"/> -->
     <Home/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from '/src/components/HelloWorld.vue'
 import Home from '/src/components/Home.vue'
 
 export default {
