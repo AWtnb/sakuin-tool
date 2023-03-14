219b0551ec549df2c127f233390f04defbb70d5c
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-14 22:32:29 +0900

    add nombre check tool

---
+++ vue/src/components/CheckNombre.vue
@@ -0,0 +1,81 @@
+<template>
+  <h2>ノンブルの並びをチェックする</h2>
+  <textarea v-model="content"></textarea>
+  <button @click="executeCheck">実行</button>
+
+  <strong>ノンブルに半角数字以外が含まれている場合は正しく判定できません。</strong>
+
+  <div>
+    <strong class="warning" v-if="message">{{ message }}</strong>
+    <ul v-if="problems.length">
+      <li v-for="(problem, idx) in problems" :key="idx">
+        <span>{{ problem.line }}</span
+        ><span v-for="(p, idx) in problem.detail" :key="idx">
+          <span :style="{ color: p.color }">←{{ p.text }}</span>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { AddressChecker } from "@/helpers/entryHandler.js";
+
+const grepInvalidNombreLine = (lines) => {
+  return lines
+    .filter((x) => String(x).trim())
+    .map((line) => {
+      const checker = new AddressChecker(line);
+      checker.checkSort();
+      checker.checkHyphen();
+      if (checker.problems.length > 0) {
+        return {
+          line: line,
+          detail: checker.problems,
+        };
+      }
+      return null;
+    })
+    .filter(Boolean);
+};
+
+export default {
+  name: "CheckNomble",
+  data: function () {
+    return {
+      content: "",
+      message: "",
+      problems: [],
+    };
+  },
+  computed: {
+    contentLines: function () {
+      return this.content.split(/\n/).map((line) => String(line));
+    },
+  },
+  methods: {
+    reset: function () {
+      this.message = "";
+      this.problems = [];
+    },
+    executeCheck: function () {
+      this.reset();
+      grepInvalidNombreLine(this.contentLines).forEach((x) => {
+        this.problems.push(x);
+      });
+      if (!this.problems.length) {
+        this.message = "問題は見当たりません！";
+      }
+      else {
+        this.message = "修正の必要があります！";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+strong.warning {
+  color: red;
+}
+</style>
--- vue/src/views/CheckView.vue
+++ vue/src/views/CheckView.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="check">
     <h1>索引のチェック</h1>
-    <CheckRefs/>
-    <hr>
-    <CheckChild/>
+    <CheckRefs />
+    <hr />
+    <CheckChild />
+    <hr />
+    <CheckNombre />
   </div>
 </template>
 
 <script>
-import CheckRefs from '@/components/CheckRefs/CheckRefs.vue'
-import CheckChild from '@/components/CheckChild.vue'
+import CheckRefs from "@/components/CheckRefs/CheckRefs.vue";
+import CheckChild from "@/components/CheckChild.vue";
+import CheckNombre from "@/components/CheckNombre.vue";
 
 export default {
-  name: 'CheckView',
+  name: "CheckView",
   components: {
     CheckRefs,
     CheckChild,
-  }
-}
+    CheckNombre,
+  },
+};
 </script>
