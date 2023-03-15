28c2ed7120cd428a65e2bdb7d91e6358b2f10057
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-15 23:21:47 +0900

    add resolce childitem tool

---
+++ vue/src/components/ResolveChild.vue
@@ -0,0 +1,72 @@
+<template>
+  <h2>子項目の復元</h2>
+  <textarea v-model="content"></textarea>
+  <button @click="executeFormat">実行</button>
+
+  <textarea placeholder="result" readonly v-model="resultStr"></textarea>
+  <CopyButton :copyStr="resultStr" />
+  <div><img src="@/assets/ResolveChild/resolveChild.png" alt="" /> </div>
+</template>
+
+<script>
+import { Entry } from "@/helpers/entryHandler.js";
+
+const resolveChildEntry = (lines) => {
+  const stack = [];
+  const parentStack = [];
+  stack.push(lines[0]);
+  parentStack.push(new Entry(lines[0]).basename);
+  for (let i = 1; i < lines.length; i++) {
+    const cur = new Entry(lines[i]);
+    const basename = cur.basename;
+    if (basename.startsWith("\u2015\u2015")) {
+      const resolved = parentStack.at(-1) + basename.substring(2);
+      stack.push(resolved + "\u3000\u3000" + cur.address);
+    } else if (basename.endsWith("\u2015\u2015")) {
+      const resolved = basename.substring(0, basename.length - 2) + parentStack.at(-1);
+      stack.push(resolved + "\u3000\u3000" + cur.address);
+    } else {
+      stack.push(cur.rawStr);
+      parentStack.push(basename);
+    }
+  }
+  return stack;
+};
+
+import CopyButton from "@/components/CopyButton.vue";
+
+export default {
+  name: "ResolveChild",
+  data: function () {
+    return {
+      content: "",
+      fmtArr: [],
+    };
+  },
+  components: {
+    CopyButton,
+  },
+  computed: {
+    contentLines: function () {
+      return this.content.split(/\n/).map((line) => String(line));
+    },
+    pureLines: function () {
+      return this.contentLines.filter((line) => line.trim()).map((line) => line.trim());
+    },
+    resultStr: function () {
+      return this.fmtArr.join("\n");
+    },
+  },
+  methods: {
+    reset: function () {
+      this.fmtArr = [];
+    },
+    executeFormat: function () {
+      this.reset();
+      resolveChildEntry(this.pureLines).forEach((x) => {
+        this.fmtArr.push(x);
+      });
+    },
+  },
+};
+</script>
--- vue/src/views/ReuseView.vue
+++ vue/src/views/ReuseView.vue
@@ -3,7 +3,7 @@
     <h1>旧版索引の再利用</h1>
     <FormatOld />
     <hr />
-    <!-- <JaConv /> -->
+    <ResolveChild />
     <hr />
     <!-- <Romanize /> -->
   </div>
@@ -11,14 +11,14 @@
 
 <script>
 import FormatOld from "@/components/FormatOld.vue";
-// import JaConv from "@/components/JaConv.vue";
+import ResolveChild from "@/components/ResolveChild.vue";
 // import Romanize from "@/components/Romanize.vue";
 
 export default {
   name: "ReuseView",
   components: {
     FormatOld,
-    // JaConv,
+    ResolveChild,
     // Romanize,
   },
 };
