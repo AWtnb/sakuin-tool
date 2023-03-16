c5bb2bd6de870a8503751739d6c27726398f505f
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-16 21:32:46 +0900

    pastebox component

---
+++ vue/src/components/PasteBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <textarea @input="onInput" placeholder="paste here!"></textarea>
+  <button @click="onClick">実行！</button>
+</template>
+
+<script>
+export default {
+  name: "PasteBox",
+  emits: ["updateContent", "buttonClicked"],
+  methods: {
+    onInput: function(evt) {
+      this.$emit("updateContent", evt);
+    },
+    onClick: function() {
+      this.$emit("buttonClicked");
+    }
+  },
+};
+</script>
