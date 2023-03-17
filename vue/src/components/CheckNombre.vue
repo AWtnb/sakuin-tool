d35bb2c3e3b272af7db5ba7a7939f4b11888e99e
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-17 22:38:41 +0900

    fix

--- vue/src/components/CheckNombre.vue
+++ vue/src/components/CheckNombre.vue
@@ -4,17 +4,15 @@
 
   <p><strong>ノンブルに半角数字以外が含まれている場合は正しく判定できません。</strong></p>
 
-  <div>
-    <strong class="warning" v-if="message">{{ message }}</strong>
-    <ul v-if="problems.length">
-      <li v-for="(problem, idx) in problems" :key="idx">
-        <span>{{ problem.line }}</span
-        ><span v-for="(p, idx) in problem.detail" :key="idx">
-          <span :style="{ color: p.color }">←{{ p.text }}</span>
-        </span>
-      </li>
-    </ul>
-  </div>
+  <strong class="warning" v-if="message">{{ message }}</strong>
+  <ul v-if="problems.length">
+    <li v-for="(problem, idx) in problems" :key="idx">
+      <span>{{ problem.line }}</span>
+      <span v-for="(p, idx) in problem.detail" :key="idx">
+        <span :style="{ color: p.color }">←{{ p.text }}</span>
+      </span>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -49,7 +47,7 @@ export default {
     };
   },
   components: {
-    PasteBox
+    PasteBox,
   },
   computed: {
     contentLines: function () {
@@ -68,8 +66,7 @@ export default {
       });
       if (!this.problems.length) {
         this.message = "問題は見当たりません！";
-      }
-      else {
+      } else {
         this.message = "修正の必要があります！";
       }
     },
