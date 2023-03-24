3628fc06eb80b1bb5e49a4509f2bee0c96f8435a
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-24 21:58:24 +0900

    composition api

--- src/components/JaConv.vue
+++ src/components/JaConv.vue
@@ -1,32 +1,22 @@
+<script setup>
+import { ref, computed } from "vue";
+
+import { Util } from "@/helpers/utils.js";
+import ResultBox from "@/components/ResultBox.vue";
+
+const content = ref("");
+const resultStr = computed(() => {
+  return content.value.replace(/[\u30a1-\u30f4\u3041-\u3093]/g, (m) => {
+    if (m.match(/[\u30a1-\u30f4]/)) {
+      return Util.toHiragana(m);
+    }
+    return Util.toKatakana(m);
+  });
+});
+</script>
+
 <template>
   <h2>カタカナひらがな相互変換</h2>
   <textarea v-model="content"></textarea>
   <ResultBox :result="resultStr" />
 </template>
-
-<script>
-import { Util } from "@/helpers/utils.js";
-import ResultBox from "@/components/ResultBox.vue";
-
-export default {
-  name: "JaConv",
-  data: function () {
-    return {
-      content: "",
-    };
-  },
-  components: {
-    ResultBox,
-  },
-  computed: {
-    resultStr: function () {
-      return this.content.replace(/[\u30a1-\u30f4\u3041-\u3093]/g, (m) => {
-        if (m.match(/[\u30a1-\u30f4]/)) {
-          return Util.toHiragana(m);
-        }
-        return Util.toKatakana(m);
-      });
-    },
-  },
-};
-</script>
