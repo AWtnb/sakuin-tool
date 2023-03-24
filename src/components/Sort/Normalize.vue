f49d9923124c90350728629b103872a6986e2b4f
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-24 19:11:13 +0900

    composition api

--- src/components/Sort/Normalize.vue
+++ src/components/Sort/Normalize.vue
@@ -1,34 +1,22 @@
+<script setup>
+import { ref, computed } from "vue";
+
+import { normalizeReading } from "@/helpers/utils";
+import ResultBox from "@/components/ResultBox.vue";
+
+const content = ref("");
+const removeNoise = ref(true);
+
+const resultStr = computed(() => {
+  return normalizeReading(content.value, removeNoise.value);
+});
+</script>
+
 <template>
   <details>
     <summary>配列読みへの変換だけ必要な場合はこちら</summary>
-    <div>
-      <input type="checkbox" id="removeNoise" v-model="removeNoise" />
-      <label for="removeNoise">カタカナ・数字・アルファベット以外を削除する</label>
-    </div>
+    <label><input type="checkbox" v-model="removeNoise" />カタカナ・数字・アルファベット以外を削除する</label>
     <textarea placeholder="paste here!" v-model="content"></textarea>
     <ResultBox :result="resultStr" />
   </details>
 </template>
-
-<script>
-import { normalizeReading } from "@/helpers/utils";
-import ResultBox from "@/components/ResultBox.vue";
-
-export default {
-  name: "Normalize",
-  data: function () {
-    return {
-      content: "",
-      removeNoise: true,
-    };
-  },
-  components: {
-    ResultBox,
-  },
-  computed: {
-    resultStr: function () {
-      return normalizeReading(this.content, this.removeNoise);
-    },
-  },
-};
-</script>
