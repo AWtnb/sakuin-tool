fec7f3455498a92bb66e61d0a4333e74b26a128a
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-24 21:41:59 +0900

    composition api

--- src/components/CheckNombre.vue
+++ src/components/CheckNombre.vue
@@ -1,21 +1,6 @@
-<template>
-  <h2>ノンブルの並びをチェックする</h2>
-  <PasteBox v-on:updateContent="content = $event.target.value" v-on:buttonClicked="executeCheck" />
+<script setup>
+import { ref, computed } from "vue";
 
-  <p><strong>ノンブルに半角数字以外が含まれている場合は正しく判定できません。</strong></p>
-
-  <strong class="warning" v-if="message">{{ message }}</strong>
-  <ul v-if="problems.length">
-    <li v-for="(problem, idx) in problems" :key="idx">
-      <span>{{ problem.line }}</span>
-      <span v-for="(p, idx) in problem.detail" :key="idx">
-        <span :style="{ color: p.color }">←{{ p.text }}</span>
-      </span>
-    </li>
-  </ul>
-</template>
-
-<script>
 import PasteBox from "@/components/PasteBox.vue";
 import { AddressChecker } from "@/helpers/addressChecker.js";
 
@@ -37,42 +22,46 @@ const grepInvalidNombreLine = (lines) => {
     .filter(Boolean);
 };
 
-export default {
-  name: "CheckNomble",
-  data: function () {
-    return {
-      content: "",
-      message: "",
-      problems: [],
-    };
-  },
-  components: {
-    PasteBox,
-  },
-  computed: {
-    contentLines: function () {
-      return this.content.split(/\n/).map((line) => String(line));
-    },
-  },
-  methods: {
-    reset: function () {
-      this.message = "";
-      this.problems = [];
-    },
-    executeCheck: function () {
-      this.reset();
-      grepInvalidNombreLine(this.contentLines).forEach((x) => {
-        this.problems.push(x);
-      });
-      if (!this.problems.length) {
-        this.message = "問題は見当たりません！";
-      } else {
-        this.message = "修正の必要があります！";
-      }
-    },
-  },
+const content = ref("");
+const message = ref("");
+const problems = ref([]);
+
+const contentLines = computed(() => {
+  return content.value.split(/\n/).map((line) => String(line));
+});
+
+const reset = () => {
+  message.value = "";
+  problems.value = [];
+};
+
+const executeCheck = () => {
+  reset();
+  problems.value = grepInvalidNombreLine(contentLines.value);
+  if (!problems.value.length) {
+    message.value = "問題は見当たりません！";
+  } else {
+    message.value = "修正の必要があります！";
+  }
 };
 </script>
+
+<template>
+  <h2>ノンブルの並びをチェックする</h2>
+  <PasteBox v-on:updateContent="content = $event.target.value" v-on:buttonClicked="executeCheck" />
+
+  <p>※ノンブルに半角数字以外が含まれている場合は正しく判定できません。</p>
+
+  <strong class="warning" v-if="message">{{ message }}</strong>
+  <ul v-if="problems.length">
+    <li v-for="(problem, idx) in problems" :key="idx">
+      <span>{{ problem.line }}</span>
+      <span v-for="(p, idx) in problem.detail" :key="idx">
+        <span :style="{ color: p.color }">←{{ p.text }}</span>
+      </span>
+    </li>
+  </ul>
+</template>
 
 <style scoped>
 strong.warning {
