ef65480cd500a036876c84808a55d97207adfb6b
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-24 21:31:51 +0900

    composition api

--- src/components/CheckChild/CheckChild.vue
+++ src/components/CheckChild/CheckChild.vue
@@ -1,46 +1,38 @@
-<template>
-  <h2>子項目候補をチェックする</h2>
-  <PasteBox v-on:updateContent="content = $event.target.value" v-on:buttonClicked="executeCheck" />
+<script setup>
+import { ref, computed } from "vue";
 
-  <div> <input type="radio" id="search-tail" value="tail" v-model="picked" /><label for="search-tail">末尾一致</label> <input type="radio" id="search-head" value="head" v-model="picked" /><label for="search-head">先頭一致</label> <input type="radio" id="search-all" value="all" v-model="picked" /><label for="search-all">両方</label> </div>
-
-  <FoundTable :found="found" />
-</template>
-
-<script>
 import PasteBox from "@/components/PasteBox.vue";
 import FoundTable from "@/components/CheckChild/FoundTable.vue";
 import { CheckChild } from "@/helpers/checkChild.js";
 
-export default {
-  name: "CheckChild",
-  data: function () {
-    return {
-      content: "",
-      picked: "all",
-      found: [],
-    };
-  },
-  components: {
-    PasteBox,
-    FoundTable,
-  },
-  computed: {
-    contentLines: function () {
-      return this.content.split(/\n/).map((line) => String(line));
-    },
-  },
-  methods: {
-    reset: function () {
-      this.found = [];
-    },
-    executeCheck: function () {
-      this.reset();
-      const checker = new CheckChild(this.contentLines, this.picked);
-      checker.findPossibles().forEach((x) => {
-        this.found.push(x);
-      });
-    },
-  },
+const content = ref("");
+const picked = ref("all");
+const found = ref([]);
+
+const contentLines = computed(() => {
+  return content.value.split(/\n/).map((line) => String(line));
+});
+
+const reset = () => {
+  found.value = [];
+};
+
+const executeCheck = () => {
+  reset();
+  const checker = new CheckChild(contentLines.value, picked.value);
+  found.value = checker.findPossibles();
 };
 </script>
+
+<template>
+  <h2>子項目候補をチェックする</h2>
+  <PasteBox v-on:updateContent="content = $event.target.value" v-on:buttonClicked="executeCheck" />
+
+  <div>
+    <label><input type="radio" value="tail" v-model="picked" />末尾一致</label>
+    <label><input type="radio" value="head" v-model="picked" />先頭一致</label>
+    <label><input type="radio" value="all" v-model="picked" />両方</label>
+  </div>
+
+  <FoundTable :found="found" />
+</template>
