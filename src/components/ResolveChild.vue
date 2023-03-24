13bd56b4868c896a22f69ebd52fca050a20f0187
awtnb_laptop <arikaebanataw@gmail.com>
2023-03-24 22:39:13 +0900

    composition api

--- src/components/ResolveChild.vue
+++ src/components/ResolveChild.vue
@@ -1,15 +1,10 @@
-<template>
-  <h2>子項目の復元</h2>
+<script setup>
+import { ref, computed } from "vue";
 
-  <label><input type="checkbox" v-model="skipHeader" />先頭行をスキップする</label>
-  <PasteBox v-on:updateContent="content = $event.target.value" v-on:buttonClicked="executeFormat"/>
-
-  <ResultBox :result="resultStr" />
-  <div><img src="@/assets/ResolveChild/resolveChild.png" alt="" /> </div>
-</template>
-
-<script>
 import { Entry } from "@/helpers/entryHandler.js";
+
+import PasteBox from "@/components/PasteBox.vue";
+import ResultBox from "@/components/ResultBox.vue";
 
 const resolveChildEntry = (lines) => {
   const stack = [];
@@ -33,47 +28,42 @@ const resolveChildEntry = (lines) => {
   return stack;
 };
 
-import PasteBox from "@/components/PasteBox.vue";
-import ResultBox from "@/components/ResultBox.vue";
+const content = ref("");
+const fmtArr = ref([]);
+const skipHeader = ref(true);
 
-export default {
-  name: "ResolveChild",
-  data: function () {
-    return {
-      content: "",
-      fmtArr: [],
-      skipHeader: true,
-    };
-  },
-  components: {
-    PasteBox,
-    ResultBox,
-  },
-  computed: {
-    contentLines: function () {
-      const lines = this.content.split(/\n/).map((line) => String(line));
-      if (this.skipHeader) {
-        return lines.slice(1);
-      }
-      return lines;
-    },
-    pureLines: function () {
-      return this.contentLines.filter((line) => line.trim()).map((line) => line.trim());
-    },
-    resultStr: function () {
-      return this.fmtArr.join("\n");
-    },
-  },
-  methods: {
-    reset: function () {
-      this.fmtArr = [];
-    },
-    executeFormat: function () {
-      this.reset();
-      resolveChildEntry(this.pureLines).forEach((x) => {
-        this.fmtArr.push(x);
-      });
-    },
-  },
-};
+const contentLines = computed(() => {
+  const lines = content.value.split(/\n/).map((line) => String(line));
+  if (skipHeader.value) {
+    return lines.slice(1);
+  }
+  return lines;
+});
+
+const pureLines = computed(() => {
+  return contentLines.value.map((line) => line.trim()).filter((line) => line.length);
+});
+
+const resultStr = computed(() => {
+  return fmtArr.value.join("\n");
+});
+
+
+const reset = () => {
+  fmtArr.value = [];
+}
+const executeFormat = () => {
+  reset();
+  fmtArr.value = resolveChildEntry(pureLines.value);
+}
 </script>
+
+<template>
+  <h2>子項目の復元</h2>
+
+  <label><input type="checkbox" v-model="skipHeader" />先頭行をスキップする</label>
+  <PasteBox v-on:updateContent="content = $event.target.value" v-on:buttonClicked="executeFormat" />
+
+  <ResultBox :result="resultStr" />
+  <div><img src="@/assets/ResolveChild/resolveChild.png" alt="" /> </div>
+</template>
