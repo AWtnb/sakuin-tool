1a8430657231789e3e43fd46e1b33a509970078f
awtnb_laptop <arikaebanataw@gmail.com>
2023-04-03 18:20:47 +0900

    make sinmpe textarea component

---
+++ src/components/SimpleTextarea.vue
@@ -0,0 +1,19 @@
+<script setup>
+const emits = defineEmits(["updateContent"]);
+
+const onInput = (evt) => {
+  emits("updateContent", evt);
+};
+</script>
+
+<template>
+  <div class="box">
+    <textarea @input="onInput" placeholder="paste here!"></textarea>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  margin: 6px auto;
+}
+</style>
